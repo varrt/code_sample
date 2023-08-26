a04e638ae9fd69add9a2866c5beebb48e539f608
pmaj <majpawel90@gmail.com>
2023-08-26 18:47:34 +0200

    chore: command bus

---
+++ src/SharedInfrastructure/CommandBus/CommandBus.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Pmaj\SampleCode\SharedInfrastructure\CommandBus;
+
+
+use Pmaj\SampleCode\SchoolDiary\Domain\Repository\GroupRepositoryInterface;
+use Pmaj\SampleCode\SchoolDiary\Infrastructure\Repository\GroupInMemoryRepository;
+
+class CommandBus
+{
+    private GroupRepositoryInterface $groupRepository;
+
+    public function __construct(
+        ?GroupRepositoryInterface $groupRepository = null
+    ) {
+        $this->groupRepository = $groupRepository ?? new GroupInMemoryRepository();
+    }
+
+    public function dispatch($command): void
+    {
+        $commandClass = get_class($command);
+        $handlerClass = $commandClass."Handler";
+        (new $handlerClass($this->groupRepository))->__invoke($command);
+    }
+}
